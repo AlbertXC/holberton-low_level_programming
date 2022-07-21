3ece0c101eed6338434c0d59e6a651348f68ded0
AlbertXC <4265@holbertonschool.com>
2022-07-21 00:16:36 +0000

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,11 +7,6 @@ int main(void)
 }
 
 /**
- * Write a C program that prints exactly
- * "Programming is like building a multilingual puzzle,
- * followed by a new line.
- *
- * Use the function puts
- * You are not allowed to use printf
- * Your program should end with the value 0
+ * main - print a message
+ * Return: ends with the value 0
  */
