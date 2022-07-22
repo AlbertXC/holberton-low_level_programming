11779308f40fc24d590b88d892a2b2608405de5d
AlbertXC <4265@holbertonschool.com>
2022-07-22 01:28:09 +0000

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- output Print alphabet in lowercase
+ *
+ * Return- return 0
+ */
+
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	
+		putchar(abc);
+	
+	putchar('\n');
+	return (0);
+}
