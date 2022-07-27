4d0bc77a2db3b7fc7d8562f8026b1de892367756
AlbertXC <4265@holbertonschool.com>
2022-07-27 23:30:21 +0000

    Sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,24 +4,26 @@
  * print_sign- Function that prints the sign of a number
  * @n: Number to be checked
  *
- * Return: 1 if c is lowercase or 0 otherwise
+ * Return: 1 if n is greater than zero
+ *		   0 if n is zero
+ *		  -1 if n is less than zero
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		_putchar(45);
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		_putchar(48);
-		return (0);
+		_putchar('-');
+		return (-1);
 	}
 }
