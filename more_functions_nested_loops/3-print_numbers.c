54d44f5ceb0f873bc34e0dfba35a50c72ecb12bc
AlbertXC <4265@holbertonschool.com>
2022-08-21 22:39:11 +0000

    The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Fuction that prints the numbers, from 0 to 9
+ *
+ * Returns: Printed numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0, num <= 9; num++)
+	{
+		_putchar(num + '0');
+	}
+
+	_putchar('\n');
+}
