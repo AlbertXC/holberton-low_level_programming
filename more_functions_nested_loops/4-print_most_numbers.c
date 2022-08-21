57f063bd1a0c5dc8778453b26e9e3eec03e8e6d9
AlbertXC <4265@holbertonschool.com>
2022-08-21 23:32:40 +0000

    I Believe In Numbers And Signs

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Function that prints the numbers, from 0 to 9, followed by a new line. Do not print 2 & 4
+ * print_most_numbers - Function that prints the numbers, from 0 to 9,
+ * followed by a new line. Do not print 2 & 4.
  *
- * Returns: Printed numbers from 0 to 9 w/o 2 & 4
+ * Returns: Printed numbers from 0 to 9 w/o 2 & 4.
  */
 
 void print_most_numbers(void)
 {
 	char num;
 
-	for(num = '0'; num <= '9'; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		if(!(num == '2' || num == '4'))
+		if (!(num == '2' || num == '4'))
 			_putchar('num');
 	}
 	_putchar('\n');
