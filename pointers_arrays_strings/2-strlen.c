61af447d740ee128bd3ea65b570279ea1c438a39
AlbertXC <4265@holbertonschool.com>
2022-09-05 20:05:55 +0000

    This report, by its very lenght, defends itself against the risk of being read

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Function that returns the
+ *			 lenght of a string
+ * @x: String to count
+ *
+ * Return: String lenght
+ */
+
+int_strlen(char *s)
+{
+	int x = 0;
+
+	while(*s[c] != '\0')
+		x++;
+
+	return x;
+}
