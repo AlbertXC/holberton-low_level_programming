badc9552e48abf3d0c7283fbb7bfbe79eb510023
AlbertXC <4265@holbertonschool.com>
2022-09-27 01:24:47 +0000

    strstr

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Function that searches a string
+ *			  for any of a aet of bytes
+ * @s: locates the 1st ocurrence in the string
+ * @accept: locates the 2nd ocurrence in the string
+ *
+ * Return: a pointer to the byte that matches one
+ *		   of the bytes, or NULL if substring is not found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int u, v;
+
+	for (u = 0; *(s + u) != '\0'; u++)
+	{
+		for (v = 0; *(accept + j) != '\0'; v++)
+		{
+			if (*(s + u) == *(accept + v))
+				return (s + u);
+		}
+	}
+	return ('\0');
+}
