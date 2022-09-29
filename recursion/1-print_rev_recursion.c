72deec1f71e49e835081b7ef9131b9f7602f1871
AlbertXC <4265@holbertonschool.com>
2022-09-29 01:14:26 +0000

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Function that returns the lenght
+ *						  of a string
+ * @s: string
+ *
+ * Return: the lenght of a string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
