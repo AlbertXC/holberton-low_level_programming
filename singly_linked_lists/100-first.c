69f8a4f31140b76cbb1a6d1313e0f789260229b3
AlbertXC <4265@holbertonschool.com>
2022-10-23 03:11:04 +0000

    The Hare and the Tortoise

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * before_main - function that prints a string before 
+ * before_main - function that prints a string before
  *				  the main function is executed.
  */
 
-void before_main(void) __attribute__ ((constructor));
+void before(void);
 {
-	printf("You're beat! and yet, you must allow, \n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
