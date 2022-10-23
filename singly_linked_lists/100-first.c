34c5a5f3134ff32876a70580b8ea59bfa1171ba2
AlbertXC <4265@holbertonschool.com>
2022-10-23 03:18:02 +0000

    The Hare and the Tortoise

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  *				  the main function is executed.
  */
 
-void before(void);
+void before(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
