5b995c6704c703990029d478a496e208a61c8187
AlbertXC <4265@holbertonschool.com>
2022-10-25 01:15:07 +0000

    Print list

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,11 @@ size_t print_listint(const listint_t *h)
 	unsigned int i;
 
 	if (h == NULL)
-	return (0);
+		return (0);
 	for (i = 0; h != NULL; i++)
 	{
 		printf("%d\n", h->n);
-	h = h->next;
+		h = h->next;
 	}
 	return (i);
 }
