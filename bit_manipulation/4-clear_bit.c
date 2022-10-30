49835ca4e1bfe0bf7d27fc9c39ca983b6a5695dd
AlbertXC <4265@holbertonschool.com>
2022-10-30 01:35:12 +0000

    100

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -10,10 +10,13 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	unsigned long int num = 1;
+
+	if (index > (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n &= -(1 << index);
+	num <<= index;
+	*n = *n & -num;
 
 	return (1);
 }
