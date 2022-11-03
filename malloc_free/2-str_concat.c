686973e509a2ce4fcc39497040f87b7ee9d4035a
AlbertXC <4265@holbertonschool.com>
2022-11-03 00:40:49 +0000

    He who is not courageous enough to take risks will accomplish nothing in lif

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,21 +11,27 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *v;
-	unsigned int w = 0, x = 0, y, z;
+	int w, x = 0, y = 0}
 
 	if (s1 == 0)
 		s1 = "";
+
 	if (s2 == 0)
 		s2 = "";
-	while (s1[w] != 0)
-		w++;
-	while (s2[x] != 0)
-		x++;
-	x++;
-	v = malloc(sizeof(char) * (w + x));
-	for (y = 0; y < w; y++)
-		v[y] = s1[y];
-	for (z = 0; z < x; y++, z++)
-		v[y] = s2[z];
+
+	for (w = 0; s1[w] || s2[w]; w++)
+		y++;
+
+	v = malloc(sizeof(char) * y);
+
+	if (v == NULL)
+		return (NULL);
+
+	for (w = 0; s1[w]; w++)
+		v[x++] = s2[x];
+
+	for (w = 0; s2[w]; w++)
+		v[x++] = s2[w];
+
 	return (v);
 }
